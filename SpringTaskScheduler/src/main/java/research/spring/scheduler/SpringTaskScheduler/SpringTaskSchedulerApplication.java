ba3bb9206376bacaccfdae71ab4db6e8ecafbf44
MinPhyoeMinThu <121812138+DejavuNerdPro@users.noreply.github.com>
2024-01-13 16:02:34 +0630

    Setup

---
+++ SpringTaskScheduler/src/main/java/research/spring/scheduler/SpringTaskScheduler/SpringTaskSchedulerApplication.java
@@ -0,0 +1,13 @@
+package research.spring.scheduler.SpringTaskScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTaskSchedulerApplication.class, args);
+	}
+
+}
---
+++ SpringTaskScheduler/src/test/java/research/spring/scheduler/SpringTaskScheduler/SpringTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package research.spring.scheduler.SpringTaskScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
